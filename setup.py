b68790e67cd39cc6a199a8a014a67e31435eacfc
shrikant9867 <41181281+shrikant9867@users.noreply.github.com>
2020-08-17 15:41:11 +0530

    [Fix] - #0000 | Setup.py changed for version-12 Setups

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-#from pip.req import parse_requirements
-from pip._internal.req import parse_requirements
-
 import re, ast
 
-# get version from __version__ variable in indictrans_crm/__init__.py
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in server/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('indictrans_crm/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+        version = str(ast.literal_eval(_version_re.search(
+                f.read().decode('utf-8')).group(1)))
 
 setup(
-	name='indictrans_crm',
-	version=version,
-	description='new flow control for crm',
-	author='khushal',
-	author_email='khushal.t@indictranstech.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+        name='indictrans_crm',
+        version=version,
+        description='new flow control for crm',
+        author='khushal',
+        author_email='khushal.t@indictranstech.com',
+        packages=find_packages(),
+        zip_safe=False,
+        include_package_data=True,
+        install_requires=install_requires
 )
