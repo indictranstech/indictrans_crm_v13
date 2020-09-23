6b3bdfe9245dad410cb5c4362944b3ce5db8b749
shrikant9867 <41181281+shrikant9867@users.noreply.github.com>
2020-09-23 16:18:45 +0530

    Update attendance.py
    [Fix] - #00000 | print Syntax changed to python 3.x.x

--- attendance.py
+++ attendance.py
@@ -6,9 +6,9 @@ import sys
 
 filepath = "attendance/WeeklyAttendance.csv"
 areader = pd.read_csv(filepath, header=0, usecols=[0,1,2,10,11,15])
-print areader.head()
+print(areader.head())
 
-print "Continue...."
+print("Continue....")
 
 import requests
 
@@ -32,27 +32,27 @@ with requests.session() as s:
         empurl = empurl + empdata
         p = s.get(url=empurl, verify=False)
         emp_data = p.json()
-        print emp_data
-        print emp_data["data"][0]["name"]
+        print(emp_data)
+        print(emp_data["data"][0]["name"])
         if row[5].strip() == "Present" or  row[5].strip() == "WeeklyOff Present" or row[5].strip() == "Present On WeeklyOff":
             attdata = '{"naming_series": "ATT-","attendance_date": "%s","company": "New Indictrans Technologies Pvt. Ltd.","status": "%s","employee": "%s", "in_time":"%s","out_time":"%s"}' %(datetime.date(), row[5].strip(),emp_data["data"][0]["name"], row[3], row[4])
-            print "AD:",attdata
+            print("AD:",attdata)
             atturl = URL + '/api/resource/Attendance';
             try:
                 p = s.post(url=atturl, data=attdata, verify=False)
-                print (p.text)
+                print(p.text)
                 try:
                     data = p.json()
                     _attdata = '{"name":"%s","docstatus":1}' %(data["data"]["name"])
                     __atturl = '/api/resource/Attendance/%s' %(data["data"]["name"])
                     _atturl = URL + __atturl
-                    print _atturl
+                    print(_atturl)
                     u = s.put(url=_atturl, data=_attdata, verify=False)
-                    print (u.text)
+                    print(u.text)
                 except:
-                    print "Error"
+                    print("Error")
             except requests.exceptions.RequestException as e:  # This is the correct syntax
-                print e
+                print(e)
 
 
     
